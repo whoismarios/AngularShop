f8558dc9a253d2d646001c39d7aad5d4928986f6
Marios Tzialidis <marios@tzialidis.com>
2023-04-11 16:58:09 +0200

    Updated Files

--- src/app/backend/files/new_user.php
+++ src/app/backend/files/new_user.php
@@ -1,5 +1,4 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
 
 $servername = "localhost";
@@ -15,7 +14,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Prepare and bind SQL statement
+// Prepare and bind SQL statement with placeholders
 $stmt = $conn->prepare("INSERT INTO user (firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
 $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
 
@@ -24,10 +23,14 @@ $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+
+// Bind parameters and execute statement
+$stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
 $stmt->execute();
 
 echo "New record created successfully";
 
+// Close statement and connection
 $stmt->close();
 $conn->close();
 ?>
